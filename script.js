fa01ddfaa6a41cad3d28a164c9af25b906897db8
userdefault13 <user.default@me.com>
2023-05-24 18:35:02 -0700

    test

--- script.js
+++ script.js
@@ -1,43 +1,141 @@
 $(document).ready(function() {
-    // Event listener for the form submission
-    $("#weather-form").submit(function(event) {
-      event.preventDefault(); // Prevent form submission from refreshing the page
-  
-      // Get the city input value
-      var city = $("#city-input").val().trim();
-  
-      // Call the function to retrieve weather data
+  // API key
+  const apiKey = "fe252cfb869d01d8dcdbd7718caecdcc";
+
+  // DOM elements
+  const $weatherForm = $("#weather-form");
+  const $cityInput = $("#city-input");
+  const $searchHistory = $("#history");
+  const $currentWeather = $("#current-weather");
+  const $forecast = $("#forecast");
+
+  // Event listener for form submission
+  $weatherForm.on("submit", function(event) {
+    event.preventDefault();
+    const city = $cityInput.val().trim();
+    if (city !== "") {
       getWeather(city);
-    });
-  
-    // Function to retrieve weather data
-    function getWeather(city) {
-      // Your API key from OpenWeatherMap
-      var apiKey = "fe252cfb869d01d8dcdbd7718caecdcc";
-  
-      // Construct the API URL
-      var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  
-      // Make the API call
-      $.ajax({
-        url: apiUrl,
-        method: "GET",
-        dataType: "json",
-        success: function(response) {
-          // Extract the necessary data from the response
-          var cityName = response.name;
-          var temperature = response.main.temp;
-          var description = response.weather[0].description;
-  
-          // Update the HTML elements with the weather data
-          $("#city-name").text(`City: ${cityName}`);
-          $("#temperature").text(`Temperature: ${temperature} K`);
-          $("#description").text(`Description: ${description}`);
-        },
-        error: function(error) {
-          console.log("Error:", error);
-        }
-      });
+      $cityInput.val("");
     }
   });
-  
+
+  // Fetch weather data from OpenWeatherMap API
+  function getWeather(city) {
+    const currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+
+    $.ajax({
+      url: currentWeatherURL,
+      method: "GET",
+      dataType: "json",
+      success: function(data) {
+        displayCurrentWeather(data);
+        saveSearchHistory(city); // Save search history
+        renderSearchHistory(); // Update search history display
+      },
+      error: function() {
+        displayError("Unable to retrieve current weather data.");
+      }
+    });
+
+    $.ajax({
+      url: forecastURL,
+      method: "GET",
+      dataType: "json",
+      success: function(data) {
+        displayForecast(data);
+      },
+      error: function() {
+        displayError("Unable to retrieve forecast data.");
+      }
+    });
+  }
+
+  // Display current weather
+  function displayCurrentWeather(data) {
+    const city = data.name;
+    const temperature = data.main.temp;
+    const description = data.weather[0].description;
+
+    $currentWeather.show();
+    $currentWeather.find("#city-name").text(city);
+    $currentWeather.find("#temperature").text(`Temperature: ${temperature}°C`);
+    $currentWeather.find("#description").text(`Description: ${description}`);
+  }
+
+  // Display forecast
+  function displayForecast(data) {
+    $forecast.empty();
+
+    for (let i = 0; i < data.list.length; i += 8) {
+      const forecastData = data.list[i];
+      const date = forecastData.dt_txt.split(" ")[0];
+      const temperature = forecastData.main.temp;
+      const description = forecastData.weather[0].description;
+
+      const $forecastItem = $("<div>", { class: "card" });
+      const $forecastCardBody = $("<div>", { class: "card-body" });
+      const $forecastDate = $("<h5>", { class: "card-title" }).text(date);
+      const $forecastTemperature = $("<p>", { class: "card-text" }).text(`Temperature: ${temperature}°C`);
+      const $forecastDescription = $("<p>", { class: "card-text" }).text(`Description: ${description}`);
+
+      $forecastCardBody.append($forecastDate, $forecastTemperature, $forecastDescription);
+      $forecastItem.append($forecastCardBody);
+      $forecast.append($forecastItem);
+    }
+  }
+
+  // Display error message
+  function displayError(message) {
+    $currentWeather.hide();
+    $forecast.empty();
+    alert(message);
+  }
+
+  // Function to save the search history to local storage
+  function saveSearchHistory(city) {
+    // Get existing search history from local storage
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+    // Add the new city to the search history if it doesn't already exist
+    if (!searchHistory.includes(city)) {
+      searchHistory.push(city);
+    }
+
+    // Save the updated search history to local storage
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  }
+
+  // Function to render the search history in the search history section
+  function renderSearchHistory() {
+    // Get the search history from local storage
+    var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+
+    // Clear the existing search history section
+    $searchHistory.empty();
+
+    // Render each city in the search history
+    for (var i = 0; i < searchHistory.length; i++) {
+      var city = searchHistory[i];
+      var $historyItem = $("<div>").addClass("list-group-item").text(city);
+
+      // Add a click event listener to the history item
+      $historyItem.on("click", function() {
+        var cityName = $(this).text();
+        searchWeather(cityName);
+      });
+
+      // Append the history item to the search history section
+      $searchHistory.append($historyItem);
+    }
+  }
+
+  // Function to search weather for a specific city from the search history
+  function searchWeather(city) {
+    $cityInput.val(city);
+    getWeather(city);
+  }
+
+  // Call the renderSearchHistory function to initially display the search history
+  renderSearchHistory();
+});
